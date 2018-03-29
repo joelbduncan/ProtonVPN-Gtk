fa453423675f1dd8e862442dfb031d820cd6f09a
Joel Duncan <joel@slethen.io>
2018-03-30 00:52:37 +0100

    Initial commit

---
+++ ProtonVPN.py
@@ -0,0 +1,110 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject
+from gi.repository import Gtk
+import subprocess
+from threading import Thread
+import socket
+import time
+import requests
+import json
+from ConfigParser import SafeConfigParser
+from serverList import serverList
+
+# Temp import for testing
+import thread
+
+# OpenVPN Gateway IP Address for connection status
+remoteServer = "10.8.8.1"
+
+# Setup GUI Handlers
+class Handler():
+
+	def __init__(self):
+		# Connect GUI components
+		self.usernameInput = builder.get_object('usernameInput')
+		self.passwordInput = builder.get_object('passwordInput')
+		self.browseServer = builder.get_object('browseServer')
+		self.statusLabel = builder.get_object('statusLabel')
+		self.locationLabel = builder.get_object('locationLabel')
+		self.ipAddressLabel = builder.get_object('ipAddressLabel')
+
+		# Populate Server list
+		for index in range(len(serverList)):
+			self.browseServer.insert(0, serverList[index][0], serverList[index][1])
+
+		# On Startup
+		# Read Config file
+		parser = SafeConfigParser()
+		parser.read('config.ini')
+
+		# Set last connected server as default
+		self.browseServer.set_active_id(parser.get('globalVars', 'lastConnection'))
+
+		# Start threading timeout for connectionStatus()
+		GObject.timeout_add_seconds(1, self.statusThread)
+
+	def statusThread(self):
+		thread = Thread(target=self.connectionStatus)
+		thread.start()
+		return True
+
+	# Check VPN connection status & upadte location infomation
+	def connectionStatus(self):
+
+		try:
+			# Update IP Address/Location
+			self.fetchIP()
+
+			# Get VPN Connection status
+			host = socket.gethostbyname(remoteServer)
+			s = socket.create_connection((host, 53), 2)
+			GObject.idle_add(self.statusLabel.set_text, str("Connected"))
+		except:
+			pass
+			GObject.idle_add(self.statusLabel.set_text, str("Disconnected"))
+
+	# Get current IP address/Location
+	def fetchIP(self):
+		send_url = 'http://freegeoip.net/json'
+		r = requests.get(send_url)
+		j = json.loads(r.text)
+		countryName = j['country_name']
+		ipAddress = j['ip']
+		
+		self.locationLabel.set_text(countryName)
+		self.ipAddressLabel.set_text(ipAddress)
+
+	# Connect to selected server
+	def connectBtn(self, button):
+		subprocess.Popen(["protonvpn-cli", "-connect", str(self.browseServer.get_active_id())])
+		parser = SafeConfigParser()
+		parser.read('config.ini')
+		parser.read('config.ini')
+		parser.set('globalVars', 'lastConnection', self.browseServer.get_active_id())
+
+		with open('config.ini', 'w') as configfile:    # save
+		    parser.write(configfile)
+
+	# Disconnect from VPN
+	def disconnectBtn(self, button):
+		subprocess.Popen(["protonvpn-cli", "-disconnect"])
+		#subprocess.Popen.kill()
+
+	def fastestServerBtn(self, button):
+		subprocess.Popen(["protonvpn-cli", "-fastest-connect"])
+
+	def randomServerBtn(self, button):
+		subprocess.Popen(["protonvpn-cli", "-random-connect"])
+
+# Connect glade GUI
+builder = Gtk.Builder()
+builder.add_from_file("proton-ui.glade")
+builder.connect_signals(Handler())
+
+# Draw window
+window = builder.get_object("ui")
+window.show_all()
+window.connect("delete-event", Gtk.main_quit)
+
+Gtk.main()
---
+++ serverList.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+'''
+To add more servers copy & paste the first line.
+The last item must end with ,]].
+'''
+
+# List of protonvpn servers
+# Will not breaking into categories maybe with a 3rd column with login level 1-3 
+serverList = [
+["ES#1", "ES#1"],
+["ES#2", "ES#2"],
+["ES#3", "ES#3"],
+["ES#4", "ES#4"],
+["CA#1", "CA#1"],
+["CA#2", "CA#2"],
+["CA#3", "CA#3"],
+["CA#4", "CA#4"],
+["DE#1", "DE#1"],
+["DE#2", "DE#2"],
+["DE#3", "DE#3"],
+["DE#4", "DE#4"],
+["US-VA#1", "US-VA#1"],
+["US-VA#2", "US-VA#2"],
+["US-VA#3", "US-VA#3"],
+["US-VA#4", "US-VA#4",]]
